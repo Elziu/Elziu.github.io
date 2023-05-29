6ceaae0a4bf606eeee027ab87b2deb6b0c8b0d2b
Jan Mielziuk <jan.jakub.mielziuk@gmail.com>
2023-05-29 14:37:09 +0200

    script.js apiRoot fix

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'https://spring-web--elziu.repl.co/v1/tasks';
+  const apiRoot = 'spring-web.elziu.repl.co/v1/tasks';
   const trelloApiRoot = 'http://localhost:8080/v1/trello';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
